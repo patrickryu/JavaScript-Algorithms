4dc60d988769135d735cc36d6521c4f8fc2b8ac1
Patrick Ryu <patrickryu1@gmail.com>
2018-07-30 18:16:53 -0300

    add-two-numbers algorithm completed and added

---
+++ Add-two-numbers/add-two-numbers.js
@@ -0,0 +1,10 @@
+function addTwoNumbers(num1, num2) {
+    return num1 + num2;
+}
+
+//Test
+console.log(addTwoNumbers(5, 6));   //Expected output 11 
+console.log(addTwoNumbers(10, 0));  //Expected output 10
+console.log(addTwoNumbers(-5, -5)); //Expected output -10
+console.log(addTwoNumbers(1, -9));  //Expected output -8
+console.log(addTwoNumbers(-5, 10)); //Expected output 5
---
+++ Add-two-numbers/readme.md
@@ -0,0 +1,6 @@
+# Ojective
+    Write a function that takes in two numbers and adds the number together.
+
+# Example returns
+* yourFunction(5, 6) will return 11
+* yourFunction(10, 0) will return 10
