91e84a1ab9ee77097ff3442823e3c2dfa2dc32ee
Patrick Ryu <patrickryu1@gmail.com>
2018-07-31 19:44:25 -0300

    celsius to fahrenheit algorithm completed and added

---
+++ 2018/convert-temperature/README.md
@@ -0,0 +1,6 @@
+# Objective
+    Write a function that convert Celsius to Fahrenheit.
+
+# Example Returns
+* myFunction(30) will return 86.
+* myFunction(-30) will return -22.
---
+++ 2018/convert-temperature/convert-temperature.js
@@ -0,0 +1,6 @@
+function celsiusToFahrenheit(celsius) {
+    return celsius * 9 / 5 + 32;;
+}
+
+console.log(celsiusToFahrenheit(30)); //Expected output: 86
+console.log(celsiusToFahrenheit(-30)); //Expected output: -22
